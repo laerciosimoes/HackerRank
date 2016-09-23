4102b517fd3361255ea6ed17b10cf91ebcb7f954
Laercio Simoes <laercio@hpcbrasil.com>
2016-09-23 16:16:54 -0300

    Initial Load

---
+++ 30_Days_Code/ConsoleApplication/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Project to Call");
+            var item = Convert.ToInt32(Console.ReadLine());
+            switch(item)
+            {
+                case 0:
+
+            }
+        }
+    }
+}
---
+++ 30_Days_Code/HackerRank/Day2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRank
+{
+    public class Day2
+    {
+        public void Main(string[] args)
+        {
+            var mealCost = Convert.ToInt32(Console.ReadLine());
+            var tipPercent = Convert.ToInt32(Console.ReadLine());
+            var taxPercent = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(getTotalCost(mealCost, tipPercent, taxPercent));
+        }
+
+        public string getTotalCost(int mealCost, int tipPercent, int taxPercent)
+        {
+            double tip = mealCost * (tipPercent / 100);
+            double tax = mealCost * (taxPercent / 100);
+            var totalCost = Math.Round(mealCost + tip + tax, 0);
+
+            return $"The total meal cost is {totalCost} dollars.";
+        }
+    }
+}
